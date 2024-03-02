814705e0ae30f26da833b21891db6b20b0dbc3ca
Lorena <160654986+5355573@users.noreply.github.com>
2024-03-02 09:08:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Hola Mundo")
+let intentos = 6;
+let diccionario = ['APPLE', 'DANCE', 'CROWN', 'LOREN']
+let palabra = "APPLE";
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+window.addEventListener('load', init)
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+const button = document.getElementById("guess-button");
+const valor = input.value;
+button.addEventListener("click", intentar);
+function intentar(){
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra ) {
+        terminar("<h1>GANASTE!😀</h1>")
+        return
+    }
+    for (let i in palabra){
+        if (INTENTO[i]===palabra[i]){
+            console.log(INTENTO[i], "VERDE")
+        } else if( palabra.includes(INTENTO[i]) ) {
+            console.log(INTENTO[i], "Amarillo")
+        } else {
+            console.log(INTENTO[i], "GRIS")
+        }
+    }
+    if (intentos==0){
+         terminar("<h1>PERDISTE!😖</h1>")
+    }
+}
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
